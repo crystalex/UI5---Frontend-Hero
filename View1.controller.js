f74fb1a97c0f03fe78682ccf86657c959e5aed02
crystalex <alexandru.zaharescu@gmail.com>
2017-08-08 14:06:25 +0200

    Update View1.controller.js

--- View1.controller.js
+++ View1.controller.js
@@ -1,21 +1,91 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/ODataModel"
+], function(Controller, ODataModel) {
 	"use strict";
 
 	return Controller.extend("com.sap.ui5.training.destination.test.destination_test.controller.View1", {
 
-		onInit: function(oEvent){
-			
+		onInit: function(oEvent) {
+
 			var that = this;
-			
-			setTimeout(function(){
-				that.getView().byId("helloWorldText").setText("bau bau");	
-				
+
+			setTimeout(function() {
+				that.getView().byId("helloWorldText").setText("bau bau");
+
 				var oViewModel = that.getView().getModel("view1Model");
 				alert(oViewModel.getProperty("/helloWorldText/text"));
 			}, 5000);
+		},
 
+		_busyDialog: null,
+
+		onMyButtonPress: function(oEvent) {
+			console.log(oEvent);
+
+			var oButton = oEvent.getSource();
+
+			console.log(oButton.data("myData"));
+
+			this.getView().byId("helloWorldText").setText(oButton.data("myData"));
+		},
+		
+				customFormatter: function(UnitsInStock, UnitsOnOrder) {
+			console.log(UnitsInStock, UnitsOnOrder)
+			return 'UnitsInStock = ' + UnitsInStock + ", UnitsOnOrder = " + UnitsOnOrder; 
+		},
+		
+		onSelectionChange: function(oEvent){
+			var oView  = this.getView();
+			var oTable = oView.byId("table1");
+			var oBindingItems = oTable.getBinding("items");
+			
+			// console.log(oBindingItems);
+			
+			var supplierId = 1;
+			var oComboBox = oEvent.getSource();
+			supplierId = oComboBox.getSelectedKey();
+			
+			var aFilters = [];
+			var newFilter = new sap.ui.model.Filter("SupplierID", sap.ui.model.FilterOperator.EQ, supplierId);
+			aFilters.push(newFilter);
+			
+			oBindingItems.filter(aFilters);
+			
+			var oSorter = new sap.ui.model.Sorter("ProductName", false , false);
+			oBindingItems.sort(oSorter);
+			
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("detail"); 
+			
+			// console.log(oBindingItems);
+		},
+
+		onLoadData: function(oEvent) {
+
+			// instantiate dialog
+			if (!this._busyDialog) {
+				this._busyDialog = sap.ui.xmlfragment("com.sap.ui5.training.destination.test.destination_test.fragments.BusyDialog", this);
+				this.getView().addDependent(this._busyDialog);
+			}
+
+			// open dialog
+			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._busyDialog);
+			this._busyDialog.open();
+
+			// odata model load
+			var oModel = new ODataModel("/northwind", {
+				serviceurl: "/northwind",
+				syncronizationMode: "None"
+			});
+
+			var that = this;
+
+			oModel.attachRequestCompleted(function() {
+				that._busyDialog.close();
+			});
+
+			this.getView().setModel(oModel, "odatamodel");
 		}
 	});
 });
