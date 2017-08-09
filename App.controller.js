7aa0139f6860507b207f9058794afc4c11cc414d
crystalex <alexandru.zaharescu@gmail.com>
2017-08-09 13:27:57 +0200

    Create App.controller.js

---
+++ App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/ODataModel"
+], function(Controller, ODataModel) {
+"use strict";
+
+	return Controller.extend("com.sap.ui5.training.destination.test.destination_test.controller.App", {
+
+	});
+});
